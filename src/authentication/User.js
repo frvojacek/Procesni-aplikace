c33e175f237139ee9a1cf316be85c0096581eeb5
František Vojáček <vojacek.fr.2021@ssps.cz>
2023-04-30 15:58:08 +0200

    Přidání vygenerovaní tokenu

--- src/authentication/User.js
+++ src/authentication/User.js
@@ -1,20 +1,23 @@
+const { randomBytes } = await import('node:crypto')
+
 export class User {
   isAuthenticated = false
 
   authenticate (email) {
     if (this.validateEmail(email, 'ssps.cz')) {
+      this.generateToken()
       this.isAuthenticated = true
     } else {
       console.log('INVALID EMAIL')
     }
   }
 
+  /*
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
+    https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+    https://web.archive.org/web/20221223174323/http://emailregex.com/
+  */
   validateEmail (email, domain) {
-    /*
-      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
-      https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
-      https://web.archive.org/web/20221223174323/http://emailregex.com/
-    */
     const expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ //eslint-disable-line
     let isValid = expression.test(email)
 
@@ -24,5 +27,16 @@ export class User {
     }
 
     return isValid
+  }
+
+  /*
+    https://metaschool.so/articles/nodejs-crypto-module/#3-generating-random-data
+    https://nodejs.org/api/crypto.html#cryptorandombytessize-callback
+  */
+  generateToken () {
+    randomBytes(10, (error, buffer) => {
+      if (error) console.error(error)
+      console.log(buffer.toString('hex'))
+    })
   }
 }
