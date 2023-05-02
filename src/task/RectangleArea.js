ed5ba4d3d536efe961f0f8b4a00fbecc62a1f472
František Vojáček <vojacek.fr.2021@ssps.cz>
2023-05-02 12:52:11 +0200

    Přidání přetečení výsledku úlohy

--- src/task/RectangleArea.js
+++ src/task/RectangleArea.js
@@ -1,15 +1,24 @@
 import { Task } from './Task.js'
 
 export class RectangleArea extends Task {
-  // https://regex-generator.olafneumann.org/?sampleText=3%2C4&flags=&selection=0%7CDigit,2%7CDigit,1%7CCharacter
-  // https://regexr.com
+  /*
+    https://regex-generator.olafneumann.org/?sampleText=3%2C4&flags=&selection=0%7CDigit,2%7CDigit,1%7CCharacter
+    https://regexr.com
+  */
   static validateSyntax (data) {
     const expression = /^\d+,\d+$/
     return expression.test(data)
   }
 
+  /*
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number#fixed-width_number_conversion
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+    https://stackoverflow.com/questions/19843652/32-bit-signed-integer-math-in-javascript
+  */
   process () {
     const [side1, side2] = this.data.split(',')
-    return side1 * side2
+    let area = side1 * side2
+    area = area > 2147483647 ? -2147483648 : area // overflow
+    return area
   }
 }
